23a353736a48cf92648c74ecfed6391b5c02edc0
deny <ndennyh@gmail.com>
2022-04-06 23:19:25 +0700

    drawable add

--- app/src/main/java/com/example/permissionactivity/MainActivity.kt
+++ app/src/main/java/com/example/permissionactivity/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.permissionactivity
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.permissionactivity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,8 +24,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.run {
-            btnShowImage.setOnClickListener{
+            btnShowImage.setOnClickListener {
                 loadImage()
+            }
+            btnNext.setOnClickListener {
+                checkPermission()
             }
         }
     }
@@ -27,5 +39,45 @@ class MainActivity : AppCompatActivity() {
             .placeholder(R.drawable.ic_launcher_background)
             .circleCrop()
             .into(image1)
+    }
+
+    private fun checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val permissionCheck = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                startActivity(Intent(this, SecondActivity::class.java)
+                    .putExtra("Location",getLatLong()))
+            } else {
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 201)
+            }
+        } else {
+            Toast.makeText(this, "Your app is not supported", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 201){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION){
+                Toast.makeText(this, "PERMISSION Granted", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, SecondActivity::class.java)
+                    .putExtra("Location",getLatLong()))
+            }else{
+                Toast.makeText(this, "PERMISSION Declined", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    @SuppressLint("MissingPermission")
+    private fun getLatLong():String {
+        val locationManager = applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        var location : Location? = null
+        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+
+        val latlong = "Lat : ${location?.latitude}, Long : ${location?.longitude}"
+        return latlong
     }
 }
--- app/src/main/java/com/example/permissionactivity/SecondActivity.kt
+++ app/src/main/java/com/example/permissionactivity/SecondActivity.kt
@@ -2,10 +2,24 @@ package com.example.permissionactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.permissionactivity.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySecondBinding
+
+    private val latlong by lazy{
+        intent.getStringExtra("Location")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.run {
+            imgBack.setOnClickListener { onBackPressed() }
+            txtLatLong.text = latlong
+        }
     }
 }
